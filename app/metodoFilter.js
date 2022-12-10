2007956a2c794433f27664f3e05991246d3561fe
Dennys Alvarenga <dennysnascimento01@gmail.com>
2022-12-10 01:34:29 -0300

    att metodoFilter.js, metodoForEach.js create metodoReduce.js finished project

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -6,4 +6,16 @@ function filtrarLivros() {
     const categoria = elementoBtn.value;
     let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
     exibirOsLivrosNaTela(livrosFiltrados);
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal)
+    }
+};
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+        <div class="livros__disponiveis">
+        <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+        </div>
+    `
 };
